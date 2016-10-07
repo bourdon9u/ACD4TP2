8f45e8a8028eae5b5a855d7ca8658d9568a0b274
bourdon9u <bourdon9u@iutsd-012-02.ad.univ-lorraine.fr>
2016-10-07 17:32:10 +0200

    Moyenne

--- ACD4-tp2/src/Exercice3/Etudiant.java
+++ ACD4-tp2/src/Exercice3/Etudiant.java
@@ -1,33 +1,77 @@
 package Exercice3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Etudiant {
 
 	private Identite id;
 	private Formation f;
-	private Map<String, ArrayList<Float>> notes;
+	private Map<String, List<Float>> notes;
 	
-	public Etudiant(Identite id, Formation f){
-		this.id=id;
-		this.f=f;
-		this.notes = new HashMap<String, ArrayList<Float>>();
+	public Etudiant(Identite id, Map<String,List<Float>> notes,Formation f ){
+		this.id = id;
+		this.notes = new HashMap<String,List<Float>>();
+		this.f =f;
 	}
-
+	public void addNote(String id, float n){
+		if(n>0 && n<=20) {
+			if(!notes.containsKey(id)){
+				notes.put(id, new ArrayList<Float>());
+			}
+			notes.get(id).add(n);
+		}		
+		else
+			System.out.println("Entrer une note entre 0 et 20");
+	}
+	
+	
+	public void Moyenne(){
+		float compteur = 0;
+		float moyenne = 0;
+	
+		if(!notes.containsKey(this))
+			System.out.println("Choisissez une matière qui existe !");
+		else
+			for(int g=0; g<notes.get(this).size(); g++){
+				compteur = compteur + notes.get(this).get(g);
+			}
+	
+		moyenne = compteur / notes.get(this).size();
+		System.out.println("La moyenne est de :");
+		
+	}
+	public void Moyennecompteur(){
+		float compteur = 0;
+		float moyenne = 0;
+		if(!notes.containsKey(this))
+			System.out.println("Cette matière n'existe pas");
+		else
+			for(int i=0; i<notes.get(this).size();i++){
+				compteur = compteur + notes.get(this).get(i);
+			}
+		
+		moyenne=compteur/notes.get(this).size();
+		if(!notes.containsKey(this))
+			System.out.println("Choisissez une matière qui existe !");
+		else
+			for(int g=0; g<notes.get(this).size(); g++){
+				compteur = compteur + notes.get(this).get(g);
+			}
+	
+		moyenne = compteur / notes.get(this).size();
+		System.out.println("La moyenne est de :");
+		
+	}
 	public Identite getId() {
 		return id;
 	}
-	
 
 	public Formation getF() {
 		return f;
 	}
 
-	public Map<String, ArrayList<Float>> getNotes() {
+	public Map<String, List<Float>> getNotes() {
 		return notes;
 	}
-	
 	
 }
